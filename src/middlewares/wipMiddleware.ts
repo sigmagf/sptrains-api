487641e7fcb30f3c6d6b7cb9ad8bd54116c96129
Joao Gabriel <jgfurlan2@gmail.com>
2020-09-13 19:19:23 -0300

    fix

--- src/middlewares/wipMiddleware.ts
+++ src/middlewares/wipMiddleware.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
 const wipMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if(process.env.NODE_ENV === 'development') {
+    return next();
+  }
+
   return res.status(401).json({ message: 'Route in development' });
 };
 
