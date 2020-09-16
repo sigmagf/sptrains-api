c4fc4a506a0fcd463d4baa62bd372f46de7c7f29
Joao Gabriel <jgfurlan2@gmail.com>
2020-09-15 23:11:08 -0300

    Explicit cors config

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 
 app.use(router);
 
