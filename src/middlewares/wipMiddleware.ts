2efc14dccfec9e717b9fe2cecd404c17ea7aaed7
Joao Gabriel <jgfurlan2@gmail.com>
2020-09-15 23:04:50 -0300

    Add delay in development

--- src/middlewares/wipMiddleware.ts
+++ src/middlewares/wipMiddleware.ts
@@ -1,8 +1,10 @@
+import delay from 'delay';
 import { NextFunction, Request, Response } from 'express';
 
-const wipMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const wipMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   if(process.env.NODE_ENV === 'development') {
-    return next();
+    delay(1000).then(() => next());
+    return null;
   }
 
   return res.status(401).json({ message: 'Route in development' });
